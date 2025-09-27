766246b103b17e12d47d1ae3b2c3eea2ecc73761
CamerG <74063919+CamerG1011@users.noreply.github.com>
2025-09-27 12:27:53 -0400

    update

--- daydream-atlanta-demo-game/Scenes/Player1.gd
+++ daydream-atlanta-demo-game/Scenes/Player1.gd
@@ -5,6 +5,10 @@ const JUMP_VELOCITY = -400.0
 const GRAVITY = 1200.0
 
 func _physics_process(delta: float) -> void:
+	
+	var idle_texture = preload("res://Assets/Player1Front.png")
+	var right_texture = preload("res://Assets/Player1Right.png")
+	var left_texture = preload("res://Assets/Player1Left.png")
 	# Apply gravity
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
